c6738385a96d2a007b93df1cad3a3f0f4489f421
dezmorales <kotmax98@yandex.ru>
2022-07-18 00:18:15 +0300

    create class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  def self.tests_by_category(name)
+    Test.joins('INNER JOIN categories ON tests.category_id = categories.id')
+        .where(categories: {title: name})
+        .pluck(:title)
+  end
 end
