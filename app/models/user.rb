cf23d70723636b8848ff2a7626300e9789e401b0
dezmorales <kotmax98@yandex.ru>
2022-07-18 15:22:28 +0300

    class User fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   def finished_tests(level)
-    Test.where(level:)
+    Test.where(level: level)
         .joins('INNER JOIN finished_tests ON tests.id = finished_tests.test_id')
         .where(finished_tests: { user_id: id })
   end
