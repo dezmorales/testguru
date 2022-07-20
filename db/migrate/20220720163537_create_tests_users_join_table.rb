f8dec3d907fd2feb0639709cbb1802b67ceeafde
dezmorales <kotmax98@yandex.ru>
2022-07-20 19:52:15 +0300

    rubocop fix

--- db/migrate/20220720163537_create_tests_users_join_table.rb
+++ db/migrate/20220720163537_create_tests_users_join_table.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateTestsUsersJoinTable < ActiveRecord::Migration[6.1]
   def change
     create_join_table :users, :tests do |t|
-    t.index [:user_id, :test_id], unique: true
-    # t.index [:test_id, :user_id]
+      t.index %i[user_id test_id], unique: true
+      # t.index [:test_id, :user_id]
     end
   end
 end
