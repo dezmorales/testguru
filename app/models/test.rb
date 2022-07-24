c73f39507086cb9e583a3cc1fdac7648103f7733
dezmorales <kotmax98@yandex.ru>
2022-07-24 18:10:07 +0300

    add scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,9 +7,13 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :author, class_name: :User, foreign_key: :author_id
 
+  scope :easy, -> {where(level: 0..1)}
+  scope :medium, -> {where(level: 2..4)}
+  scope :hard, -> {where(level: 5..Float::INFINITY)}
+
+  scope :category_name, -> (name) {joins(:category).where(category: {title: name})}
+
   def self.tests_by_category(name)
-    Test.joins(:category)
-        .where(category: { title: name })
-        .order(title: :desc).pluck(:title)
+    category_name(name).order(title: :desc).pluck(:title)
   end
 end
