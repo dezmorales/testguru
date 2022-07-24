43802e9bdbdb6ba5be0cd97f3bceb67929b657f9
dezmorales <kotmax98@yandex.ru>
2022-07-24 18:57:17 +0300

    rubocop fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,10 +3,10 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :correct_answer, -> {where(correct: true)}
+  scope :correct_answer, -> { where(correct: true) }
 
   validates :body, presence: true
-  validates :validate_number_of_answers
+  validate :validate_number_of_answers
 
   private
 
--- app/models/test.rb
+++ app/models/test.rb
@@ -8,13 +8,13 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id
 
   validates :title, presence: true, uniqueness: { scope: :level }
-  validates :level, numericality: {only_integer: true, greater_than: 0}
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
 
-  scope :easy, -> {where(level: 0..1)}
-  scope :medium, -> {where(level: 2..4)}
-  scope :hard, -> {where(level: 5..Float::INFINITY)}
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
 
-  scope :category_name, -> (name) {joins(:category).where(category: {title: name})}
+  scope :category_name, ->(name) { joins(:category).where(category: { title: name }) }
 
   def self.tests_by_category(name)
     category_name(name).order(title: :desc).pluck(:title)
