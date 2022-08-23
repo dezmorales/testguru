610bb764fa92ef87b8e827a58d1ca43c7cca478d
dezmorales <kotmax98@yandex.ru>
2022-08-23 10:45:52 +0300

    library devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+
   root to: 'pages#home'
-
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#destroy'
-
-  resources :users, only: :create
-  resources :sessions, only: :create
 
   resources :answers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
@@ -27,5 +22,9 @@ Rails.application.routes.draw do
     member do
       get :result
     end
+  end
+
+  namespace :admin do
+    resources :tests
   end
 end
